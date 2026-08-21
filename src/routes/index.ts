ef739001d430fa8f4f46b4da7739ca526ebe6bda
mochsheva56-web <mochsheva56@gmail.com>
2026-08-21 11:08:15 +0700

    fix: resolve eslint errors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,12 @@
 import { Router } from 'express';
-feat/task-5
 import authRoute from './auth.route';
 import reportingRoute from './reporting.route';
-
-import auth from './auth.route';
 import dashboard from './dashboard.route';
-main
 
 const router = Router();
 
 router.use('/auth', authRoute);
 router.use('/reports', reportingRoute);
-
 router.use('/dashboard', dashboard);
 
 export default router;
